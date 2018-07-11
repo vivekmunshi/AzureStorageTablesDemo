f838db08cf8ae715c06d6a7f7678aabae75ccb9d
Palmeto <Palmeto@DESKTOP-2R1O2ST>
2018-07-11 16:52:58 +0530

    check-in with updated code

--- AzureStorageTablesDemo/Program.cs
+++ AzureStorageTablesDemo/Program.cs
@@ -21,16 +21,58 @@ namespace AzureStorageTablesDemo
             Console.WriteLine("Table customers is ready!");
 
             //Insert 
-            CustomerEntity customer1 = new CustomerEntity("AP", "A01");
-            customer1.LastName = "Mark";
-            customer1.FirstName = "wines";
-            customer1.Email = "markl@gmail.com";
-            customer1.PhoneNumber = "9100945142";
+            //CustomerEntity customer1 = new CustomerEntity("AP", "A01");
+            //customer1.LastName = "Mark";
+            //customer1.FirstName = "wines";
+            //customer1.Email = "markl@gmail.com";
+            //customer1.PhoneNumber = "9100945142";
 
 
-            TableOperation insertOperation =  TableOperation.Insert(customer1);
-            table.Execute(insertOperation);
-            Console.WriteLine("Row Added!");
+            //TableOperation insertOperation =  TableOperation.Insert(customer1);
+            //table.Execute(insertOperation);
+            //Console.WriteLine("Row Added!");
+
+            //TableBatchOperation batchOperation = new TableBatchOperation();
+            //CustomerEntity customer1 = new CustomerEntity("MH", "A01");
+            //customer1.LastName = "Mark";
+            //customer1.FirstName = "wines";
+            //customer1.Email = "markl@gmail.com";
+            //customer1.PhoneNumber = "9100945142";
+
+            //CustomerEntity customer2 = new CustomerEntity("MH", "A02");
+            //customer2.LastName = "Mark";
+            //customer2.FirstName = "wines";
+            //customer2.Email = "markl@gmail.com";
+            //customer2.PhoneNumber = "9100945142";
+
+
+            //CustomerEntity customer3 = new CustomerEntity("MH", "A03");
+            //customer3.LastName = "Mark";
+            //customer3.FirstName = "wines";
+            //customer3.Email = "markl@gmail.com";
+            //customer3.PhoneNumber = "9100945142";
+
+            //CustomerEntity customer4 = new CustomerEntity("MH", "A04");
+            //customer4.LastName = "Mark";
+            //customer4.FirstName = "wines";
+            //customer4.Email = "markl@gmail.com";
+            //customer4.PhoneNumber = "9100945142";
+
+
+            //batchOperation.Insert(customer1);
+            //batchOperation.Insert(customer2);
+            //batchOperation.Insert(customer3);
+            //batchOperation.Insert(customer4);
+
+            //table.ExecuteBatch(batchOperation);
+
+            TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "MH"));
+            foreach (var item in table.ExecuteQuery(query))
+            {
+                Console.WriteLine($"{ item.FirstName }{item.LastName}{item.PhoneNumber}");
+            }
+            Console.WriteLine("Completed");
+            Console.ReadLine();
 
         }
 
