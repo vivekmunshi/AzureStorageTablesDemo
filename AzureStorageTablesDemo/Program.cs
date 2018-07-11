ab34372b33eb8ecc1a9c29d354567a4a8e7547a7
Palmeto <Palmeto@DESKTOP-2R1O2ST>
2018-07-11 16:14:37 +0530

    Add project files.

---
+++ AzureStorageTablesDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureStorageTablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storedAccount = CloudStorageAccount.Parse
+                                                (CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudTableClient tableClient = storedAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("customers");
+            table.CreateIfNotExists();
+
+        }
+    }
+}
