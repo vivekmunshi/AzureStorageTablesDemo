5dad018b53165888f98d3c8fb2bfb4fb71732caf
Palmeto <Palmeto@DESKTOP-2R1O2ST>
2018-07-11 16:31:06 +0530

    chnages after running

--- AzureStorageTablesDemo/Program.cs
+++ AzureStorageTablesDemo/Program.cs
@@ -18,7 +18,38 @@ namespace AzureStorageTablesDemo
             CloudTableClient tableClient = storedAccount.CreateCloudTableClient();
             CloudTable table = tableClient.GetTableReference("customers");
             table.CreateIfNotExists();
+            Console.WriteLine("Table customers is ready!");
 
+            //Insert 
+            CustomerEntity customer1 = new CustomerEntity("AP", "A01");
+            customer1.LastName = "Mark";
+            customer1.FirstName = "wines";
+            customer1.Email = "markl@gmail.com";
+            customer1.PhoneNumber = "9100945142";
+
+
+            TableOperation insertOperation =  TableOperation.Insert(customer1);
+            table.Execute(insertOperation);
+            Console.WriteLine("Row Added!");
+
+        }
+
+        public class CustomerEntity:TableEntity
+        {
+            public CustomerEntity(string state,string customerId)
+            {
+                this.PartitionKey = state;
+                this.RowKey = customerId;
+            }
+            public CustomerEntity()
+            {
+
+            }
+
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
         }
     }
 }
